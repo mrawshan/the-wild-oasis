a946c4947f536b4f6ffb19686965c08cb6e1c69a
rawshandelvify <rawshan@delvify.ai>
2026-04-28 03:52:56 +0530

    Supabase backup update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 // Conecting the supabase database with our app
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://akvdlbsrieoffwhjgihc.supabase.co';
+export const supabaseUrl = 'https://plkuwzknejrnsmzxqeni.supabase.co';
 const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrdmRsYnNyaWVvZmZ3aGpnaWhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA2MTkxMTEsImV4cCI6MjA0NjE5NTExMX0.6WaTKOHmihLL2R2JvH8GZttONzDQ_jI-ak8jb6iPdg4';
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsa3V3emtuZWpybnNtenhxZW5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxMjIxMjMsImV4cCI6MjA5MjY5ODEyM30.HVuXFLh2kXWElbU85bXHmBT7UGbb4VwbZcCTdfgthNI';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
